2cb118e47555c11ca4f0a530e2239aa0efc523e9
Shifuro <angleoftail@gmail.com>
2025-01-24 20:22:26 +0700

    0.0.3
    Put image & fix js

--- java.js
+++ java.js
@@ -1,18 +1,5 @@
-//var x = Math.floor(Math.random()*62 +62);
-let a = document.getElementById("loading");
 
-let i =0;
 
-//loading 
-for(let i=0;i<=54;++i){
-    let x = Math.floor(Math.random()*62 +62);
-    document.write(String.fromCharCode(x));
-    
-    for(let j=0;j<=140;++j){
-        document.write(" ");
-        let x = Math.floor(Math.random()*62 +62);
-        document.write(String.fromCharCode(x));
-
-    }
-    document.write("<br>")
-}
+/*setTimeout(() => {
+    navbarEl.classList.remove('loading');
+}, 1000);*/
